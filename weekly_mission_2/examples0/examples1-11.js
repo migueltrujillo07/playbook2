de4788cf580f2c19ee13901125a83317426b429e
Mike <miguetrulo07@gmail.com>
2022-04-17 22:46:08 -0500

    Ejemplo 11

---
+++ weekly_mission_2/examples0/examples1-11.js
@@ -0,0 +1,6 @@
+// Ejemplo 11: Uso de find en una lista de objetos
+
+const ages = [24,22, 19, 25, 32, 35, 18]
+const age = ages.find((age) => age < 20)
+console.log("Ejemplo 11: Primera edad menor a 20 es:" + age )
+
