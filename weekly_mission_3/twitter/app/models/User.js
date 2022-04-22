ecceca16f8a9d9213631a091fa3659ed5883741e
Mike <miguetrulo07@gmail.com>
2022-04-21 23:10:50 -0500

    añadiendo condigo de user y user.tes

---
+++ weekly_mission_3/twitter/app/models/User.js
@@ -0,0 +1,12 @@
+class User {
+    constructor(id,username,name,bio,dateCreated,lastUpdate){        
+        this.id = id
+        this.username = username
+        this.name = name
+        this.bio = bio
+        this.dateCreated = dateCreated
+        this.lastUpdate = lastUpdate
+    }
+}
+
+module.exports = User
---
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -0,0 +1,19 @@
+// `id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`. 
+const User = require('./../../app/models/user')
+describe("Init TEST for user class", () =>{
+    test('Create an user onject ',() =>{
+       // Aqui invocas el codigo que vas a usar en nuestra app :) 
+        const user = new user(1, "MiguelLopez", "Miguel","Mechatronics in process","datecreated","lastupdate")
+        //Aqui se validan los resultados de este codigo 
+        //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha(valorEsperado)
+        expect(user.id)toBe(1)
+        expect(user.username)toBe("MiguelLopez")
+        expect(user.name)toBe("Miguel")
+        expect(user.bio)toBe("Mechatronics in process")
+        expect(user.dateCreated)toBe("datecreated")
+        expect(user.lastUpdate)toBe("lastupdate")
+
+    })
+
+
+})
