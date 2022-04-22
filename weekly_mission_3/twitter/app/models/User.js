0b1e8f722ed6ae02897709268c600d40717a3551
Mike <miguetrulo07@gmail.com>
2022-04-21 23:43:52 -0500

    Prueba que falla aproposito B)

--- weekly_mission_3/twitter/app/models/User.js
+++ weekly_mission_3/twitter/app/models/User.js
@@ -1,11 +1,11 @@
 class User {
     constructor(id,username,name,bio,dateCreated,lastUpdate){        
-        this.id = id
-        this.username = username
-        this.name = name
-        this.bio = bio
-        this.dateCreated = dateCreated
-        this.lastUpdate = lastUpdate
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.bio = bio;
+        this.dateCreated = dateCreated;
+        this.lastUpdate = lastUpdate;
     }
 }
 
--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -1,17 +1,20 @@
 // `id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`. 
-const User = require('./../../app/models/user')
+const User = require('./../../app/models/User')
 describe("Init TEST for user class", () =>{
     test('Create an user onject ',() =>{
+
        // Aqui invocas el codigo que vas a usar en nuestra app :) 
-        const user = new user(1, "MiguelLopez", "Miguel","Mechatronics in process","datecreated","lastupdate")
+
+        const user = new  User(1, "MiguelLopez", "Miguel","Mechatronics in process","datecreated","lastupdate")
         //Aqui se validan los resultados de este codigo 
         //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha(valorEsperado)
-        expect(user.id)toBe(1)
-        expect(user.username)toBe("MiguelLopez")
-        expect(user.name)toBe("Miguel")
-        expect(user.bio)toBe("Mechatronics in process")
-        expect(user.dateCreated)toBe("datecreated")
-        expect(user.lastUpdate)toBe("lastupdate")
+        
+        expect(user.id).toBe(3)
+        expect(user.username).toBe("MiguelLopez")
+        expect(user.name).toBe("Miguel")
+        expect(user.bio).toBe("Mechatronics in process")
+        expect(user.dateCreated).toBe("datecreated")
+        expect(user.lastUpdate).toBe("lastupdate")
 
     })
 
