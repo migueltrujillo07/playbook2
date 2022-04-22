8888c9c51cad57bcf6afaca6da8040950b470c2c
Mike <miguetrulo07@gmail.com>
2022-04-21 20:45:54 -0500

    Ya quedo TDD, esta muy interesante la debis :)

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -5,13 +5,13 @@ describe("Unit Tests for spiderman class", () => {
   test('1) Create an spiderman object', () => {
       // Aquí escribimos el codigo que queremos usar tal cual
       // Quiero poder instanciar un objeto Spiderman con esta información
-      const andrewGarfield = new Spiderman("Spiderman Sony",31,"Andrew Garfield", 2,"Sony")
+      const andrewGarfield = new Spiderman("Spiderman Sonny",31,"Andrew Garfield",2,"Sony")
       
       // Validamos que este codigo funcione de la forma esperada
       expect(andrewGarfield.name).toBe("Spiderman Sonny")
       expect(andrewGarfield.age).toBe(31)
       expect(andrewGarfield.actor).toBe("Andrew Garfield")
-      expect(andrewGarfield.movies).toBe(31)
+      expect(andrewGarfield.movies).toBe(2)
       expect(andrewGarfield.studio).toBe("Sony")
     
   });
