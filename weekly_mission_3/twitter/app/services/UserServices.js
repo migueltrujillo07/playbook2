c7cf99c3fd3c01df5203162bf8eee90377cdd8af
Mike <miguetrulo07@gmail.com>
2022-04-24 10:37:53 -0500

    Ya quedo el segundo requerimiento :D, gracias a las dudas del disccutions

--- weekly_mission_3/twitter/app/services/UserServices.js
+++ weekly_mission_3/twitter/app/services/UserServices.js
@@ -4,7 +4,9 @@ class UserService {
     static create(id, username, name){
         return new User(id, username, name, "Sin bio")
     }
-    static getInfo()
+    static getInfo(user){
+        return [user.id, user.username, user.name, "sin bio"];
+    }
 }
 
 module.exports = UserService
