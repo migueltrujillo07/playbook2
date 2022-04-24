edcad0864cd3f58ff0e21f904a7be10a74f8eb46
Mike <miguetrulo07@gmail.com>
2022-04-23 23:18:15 -0500

    No me sale el segundo requerimiento del user-service

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -14,5 +14,16 @@ describe("Test for services", () => {
 
     })
 
+    test("2. Get all user data in a list",() =>{
+
+        const user = UserService.create(1, "migeltrujillo", "miguel")
+        const userInfoInList = UserService.getInfo(user)
+        expect(user.InfoInList[0]).toBe(1)
+        expect(user.InfoInList[1]).toBe("migueltrujillo")
+        expect(userInfoInList[2]).toBe("miguel")
+        expect(userInfoInList[3]).toBe("sin bio")
+        
+
+    })
 
 })
