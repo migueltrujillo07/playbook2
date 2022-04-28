0267272aeb4b94392c85d4415c2f59564fec1af5
Mike <miguetrulo07@gmail.com>
2022-04-27 23:38:42 -0500

    Creating an endpoint the get back a list of explorers, it's not finish c u 2morrow

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -8,6 +8,12 @@ app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
 
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET ALL request ${new Date()}`)
+    const explorer1 = {id: 1, name: "Miguel1"}
+
+})
+
 //Con esto inicializamos esta app
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
