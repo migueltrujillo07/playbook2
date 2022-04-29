8de626908f2a511b24d94e37aad5ade3bd5d724c
Mike <miguetrulo07@gmail.com>
2022-04-28 23:34:20 -0500

    GET Creating an endpoint that get back an explorer mediando an ID, si me salen las practicas :D

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -19,6 +19,14 @@ app.get('/v1/explorers', (req, res) => {
 
 })
 
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id : 1, name: "Carlo"}
+    res.status(200).json(explorer)
+
+})
+
 //Con esto inicializamos esta app
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
