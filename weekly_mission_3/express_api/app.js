a6c63263d7657af4e78598078db544411b4eeba2
Mike <miguetrulo07@gmail.com>
2022-04-28 22:46:27 -0500

    Creatring an endpoint

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -14,7 +14,7 @@ app.get('/v1/explorers', (req, res) => {
     const explorer2 = {id: 2, name: "Miguel2"}
     const explorer3 = {id: 3, name: "Miguel3"}
     const explorer4 = {id: 4, name: "Miguel4"}
-    const explorer = [explorer1, explorer2, explorer3, explorer4]
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
 
 })
