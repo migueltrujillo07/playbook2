db79bb92581b0403685fe9b5e35baada3dc1af26
Mike <miguetrulo07@gmail.com>
2022-04-29 23:16:00 -0500

    Practica finalizada, eso espero :D

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -39,6 +39,13 @@ app.put('/v1/explorers/:id', (req, res) => {
     res.status(200).json({message: "Update"})
 
 })
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers DELETE request ${new Date()}`)
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body // Parametros para un cliente
+    res.status(200).json({message: "Delete"})
+
+})
 
 //Con esto inicializamos esta app
 app.listen(port, () =>{
